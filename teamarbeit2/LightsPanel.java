6d9d00696c7ed8e15c8d893c6597ec62dabfcff8
JohannesUcel <u.johannes@hotmail.com>
2014-12-11 20:33:51 +0100

    Just an Update

--- teamarbeit2/LightsPanel.java
+++ teamarbeit2/LightsPanel.java
@@ -29,10 +29,43 @@ public class LightsPanel extends JPanel{
 			
 				buttons[i] = new JButton();
 				this.add(buttons[i]);
+				buttons[i].addActionListener(lc);
 			}
 		}
 		
 	
+	/**
+	 * @return the lc
+	 */
+	public Controller getLc() {
+		return lc;
+	}
+
+
+	/**
+	 * @param lc the lc to set
+	 */
+	public void setLc(Controller lc) {
+		this.lc = lc;
+	}
+
+
+	/**
+	 * @return the restart
+	 */
+	public JButton getRestart() {
+		return restart;
+	}
+
+
+	/**
+	 * @param restart the restart to set
+	 */
+	public void setRestart(JButton restart) {
+		this.restart = restart;
+	}
+
+
 	/**
 	 * @return the buttons
 	 */
