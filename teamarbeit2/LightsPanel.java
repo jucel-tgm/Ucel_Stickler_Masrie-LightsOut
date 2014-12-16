6856ed47872aa680c4194dd7858eb77e8bbfbe61
Matthias Stickler <mstickler@student.tgm.ac.at>
2014-12-16 13:31:47 +0100

    Feld anpassbar + Buttondesign

--- teamarbeit2/LightsPanel.java
+++ teamarbeit2/LightsPanel.java
@@ -19,20 +19,56 @@ public class LightsPanel extends JPanel{
 	/**
 	 * 
 	 */
-	public LightsPanel(Controller lc) {
-		this.setLayout(new GridLayout(5,5));
+	public LightsPanel(Controller lc, int fieldSize) {
+		this.setLayout(new GridLayout(fieldSize+1,fieldSize,4,4));
 		this.lc = lc;
-		buttons = new JButton[25];
+		buttons = new JButton[fieldSize*fieldSize];
 		restart = new JButton("Neustart");
 		
 		for(int i = 0; i<buttons.length;i++){
-			
 				buttons[i] = new JButton();
+				buttons[i].addActionListener(lc);
+				buttons[i].setBackground(Color.GREEN);
+				buttons[i].setOpaque(true);
 				this.add(buttons[i]);
 			}
-		}
-		
+		restart = new JButton("Restart");
+		restart.addActionListener(lc);
+		this.add(restart);
+	}
 	
+	/**
+	 * @return the lc
+	 */
+	public Controller getLc() {
+		return lc;
+	}
+
+
+	/**
+	 * @param lc the lc to set
+	 */
+	public void setLc(Controller lc) {
+		this.lc = lc;
+	}
+
+
+	/**
+	 * @return the restart
+	 */
+	public JButton getRestart() {
+		return restart;
+	}
+
+
+	/**
+	 * @param restart the restart to set
+	 */
+	public void setRestart(JButton restart) {
+		this.restart = restart;
+	}
+
+
 	/**
 	 * @return the buttons
 	 */
