aa6485973fddba711290246ea1c5000247e0f787
JohannesUcel <u.johannes@hotmail.com>
2014-12-17 08:45:26 +0100

    Constructor Java Doc

--- teamarbeit2/LightsPanel.java
+++ teamarbeit2/LightsPanel.java
@@ -1,5 +1,7 @@
 package teamarbeit2;
+
 import java.awt.*;
+
 import javax.swing.*;
 
 /**
@@ -7,36 +9,74 @@ import javax.swing.*;
  */
 
 /**
- * View Klasse 
+ * View Klasse
  * 
  * @author Matthias Stickler
  * @version 1.0
  */
-public class LightsPanel extends JPanel{
+public class LightsPanel extends JPanel {
 	private JButton[] buttons;
 	private Controller lc;
 	private JButton restart;
+	private JLabel moves;
+	private int moveCounter;
+
 	/**
-	 * 
+	 * Konstruktor zum Initialisieren
 	 */
 	public LightsPanel(Controller lc, int fieldSize) {
-		this.setLayout(new GridLayout(fieldSize+1,fieldSize,4,4));
+		this.setLayout(new GridLayout(fieldSize + 1, fieldSize, 4, 4));
 		this.lc = lc;
-		buttons = new JButton[fieldSize*fieldSize];
+		buttons = new JButton[fieldSize * fieldSize];
 		restart = new JButton("Neustart");
-		
-		for(int i = 0; i<buttons.length;i++){
-				buttons[i] = new JButton();
-				buttons[i].addActionListener(lc);
-				buttons[i].setBackground(Color.GREEN);
-				buttons[i].setOpaque(true);
-				this.add(buttons[i]);
-			}
+
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i] = new JButton();
+			buttons[i].addActionListener(lc);
+			buttons[i].setBackground(Color.GREEN);
+			buttons[i].setOpaque(true);
+			this.add(buttons[i]);
+		}
 		restart = new JButton("Restart");
 		restart.addActionListener(lc);
 		this.add(restart);
+		moveCounter = 0;
+		moves = new JLabel("Moves: " + moveCounter);
+		this.add(new JLabel());
+		this.add(moves);
 	}
-	
+
+	/**
+	 * @return the moves
+	 */
+	public JLabel getMoves() {
+		return moves;
+	}
+
+	/**
+	 * @param moves
+	 *            the moves to set
+	 */
+	public void setMoves(JLabel moves) {
+		this.moves = moves;
+	}
+
+	/**
+	 * @return the moveCounter
+	 */
+	public int getMoveCounter() {
+		return moveCounter;
+	}
+
+	/**
+	 * @param moveCounter
+	 *            the moveCounter to set
+	 */
+	public void setMoveCounter(int moveCounter) {
+		this.moveCounter = moveCounter;
+		this.repaint();
+	}
+
 	/**
 	 * @return the lc
 	 */
@@ -44,14 +84,13 @@ public class LightsPanel extends JPanel{
 		return lc;
 	}
 
-
 	/**
-	 * @param lc the lc to set
+	 * @param lc
+	 *            the lc to set
 	 */
 	public void setLc(Controller lc) {
 		this.lc = lc;
 	}
-
 
 	/**
 	 * @return the restart
@@ -60,14 +99,13 @@ public class LightsPanel extends JPanel{
 		return restart;
 	}
 
-
 	/**
-	 * @param restart the restart to set
+	 * @param restart
+	 *            the restart to set
 	 */
 	public void setRestart(JButton restart) {
 		this.restart = restart;
 	}
-
 
 	/**
 	 * @return the buttons
@@ -75,8 +113,10 @@ public class LightsPanel extends JPanel{
 	public JButton[] getButtons() {
 		return buttons;
 	}
+
 	/**
-	 * @param buttons the buttons to set
+	 * @param buttons
+	 *            the buttons to set
 	 */
 	public void setButtons(JButton[] buttons) {
 		this.buttons = buttons;
